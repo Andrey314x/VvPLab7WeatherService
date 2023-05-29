6c30aa0f7bce40555ffb918bc1fdacd9c0ef041e
”Andrey314x” <andryuhich2009@gmail.com>
2023-05-29 08:51:51 +0300

    Class weather initialized

---
+++ WeatherService/Weather.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Weather
+{
+	std::string city; //  Киров
+	double lon; // 49.6601
+	double lat; // 58.5966
+	double temperature; // 5.69
+	std::string weather; // дождь
+	double windSpeed; // 4.27
+	int clouds; // 100
+
+public:
+	Weather(string city, double lon, double lat, double temp, string weather, double windspeed, int clouds) {
+		this->city = city;
+		this->lon = lon;
+		this->lat = lat;
+		this->temperature = temp;
+		this->weather = weather;
+		this->windSpeed = windspeed;
+		this->clouds = clouds;
+	}
+
+};
+

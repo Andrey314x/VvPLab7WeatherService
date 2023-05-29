9a871b3629d7d063cc1ce5c64cba5a036bdffcad
”Andrey314x” <andryuhich2009@gmail.com>
2023-05-29 11:28:18 +0300

    XML Service improved

--- WeatherService/XMLService.cpp
+++ WeatherService/XMLService.cpp
@@ -10,15 +10,15 @@ Weather XmlService::getWeather(std::string s)
     xml_parse_result result = doc.load_file(s.c_str());
     if (!result)
         throw exception("error");
-    xml_node node = doc;
-    std::string city = node.child("city").attribute("name").as_string(); //  Киров
-    double lon = node.child("coord").attribute("lon").as_int(); // 49.6601
-    double	lat = node.child("coord").attribute("lat").as_int(); // 58.5966
-
     xml_node node = doc.child("current");
+    
 
-    double temperature = node.child("temperature").attribute("registerNumber").as_double(); // 5.69
-    std::string weather = node.child("weather").attribute("value").as_string(); // дождь
+    string city = node.child("city").attribute("name").as_string(); //  Киров
+    double lon = node.child("city").child("coord").attribute("lon").as_float(); // 49.6601
+    double lat = node.child("city").child("coord").attribute("lat").as_float(); // 58.5966
+
+    double temperature = node.child("temperature").attribute("value").as_double(); // 5.69
+    string weather = node.child("weather").attribute("value").as_string(); // дождь
     double windSpeed = node.child("wind").child("speed").attribute("value").as_double(); // 4.27
     int clouds = node.child("clouds").attribute("value").as_int(); // 100
     return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
